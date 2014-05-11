92cb65aea05a298671c59e7df361ac3cb768b005
Samy Pessé <samypesse@gmail.com>
2014-05-11 17:14:43 +0200

    Base generation

--- README.md
+++ README.md
@@ -2,3 +2,30 @@ Cover Generation for GitBook
 ================
 
 Generate a cover for the book.
+
+## Installation
+
+This module use [node-canvas](https://github.com/LearnBoost/node-canvas). You need to install some modules on your system before being able to use it: [Wiki of node-canvas](https://github.com/LearnBoost/node-canvas/wiki/_pages).
+
+## Configuration
+
+Here is default configuration of **autocover**, you can change it in your book.json:
+
+```js
+{
+    "title": "My Book",
+    "author": "Author",
+    "font": {
+        "size": null,
+        "family": "Impact",
+        "color": '#FFF'
+    },
+    "size": {
+        "w": 1600,
+        "h": 2400
+    },
+    "background": {
+        "color": '#09F'
+    }
+}
+```
--- index.js
+++ index.js
@@ -1,8 +1,73 @@
+var fs = require('fs');
+var path = require('path');
+var _ = require('lodash');
+var Q = require('q');
+var Canvas = require('canvas');
+
+var createCover = function(output, options) {
+    var d = Q.defer();
+
+    options = _.defaults(options || {}, {
+        "title": "My Book",
+        "author": "Author",
+        "font": {
+            "size": null,
+            "family": "Impact",
+            "color": '#FFF'
+        },
+        "size": {
+            "w": 1600,
+            "h": 2400
+        },
+        "background": {
+            "color": '#09F'
+        }
+    });
+    options.font.size = options.font.size || (options.size.w/(options.title.length/2));
+    console.log(options);
+
+    var canvas = new Canvas(options.size.w, options.size.h);
+
+    var ctx = canvas.getContext('2d');
+
+    // Background
+    ctx.fillStyle = options.background.color;
+    ctx.fillRect(0, 0, options.size.w, options.size.h);
+
+    // Title
+    ctx.fillStyle = options.font.color;
+    ctx.font = options.font.size+"px "+options.font.family;
+    ctx.fillText(options.title, 50, options.font.size);
+
+
+
+    var out = fs.createWriteStream(output);
+    var stream = canvas.createPNGStream();
+
+    stream.on('data', function(chunk){
+        out.write(chunk);
+    });
+    stream.on('end', function() {
+        d.resolve();
+    });
+
+    return d.promise;
+};
+
 module.exports = {
     book: {},
     hooks: {
         "finish": function() {
-            // Generate cover here
+            // Check if a cover already exists in the input
+            if (fs.existsSync(path.join(this.options.input, "book.jpg"))
+            || fs.existsSync(path.join(this.options.input, "book.png"))) {
+                return;
+            }
+
+            // Generate cover
+            return createCover(_.extend({}, {
+                title: this.options.title
+            }, this.config.pluginsConfig.autocover));
         }
     }
 };

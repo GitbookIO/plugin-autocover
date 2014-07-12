cf281ec99d05986a8b056cbacdcae00e72d9608c
Aaron O'Mullan <aaron.omullan@friendco.de>
2014-07-12 16:14:42 -0700

    Add topic detection module

---
+++ topic.js
@@ -0,0 +1,35 @@
+var topics = require('./topics.json');
+
+// Break up the incomming text into tokens
+function tokenize(str) {
+    return str.toLowercase().split(/\s+/).filter(Boolean);
+}
+
+// Intersection of two arrays
+function intersect(a, b) {
+    var t;
+    // Swap a and b (to use shortest for loop iteration)
+    if (b.length > a.length) {
+        t = b, b = a, a = t;
+    }
+
+    return a.filter(function (e) {
+        if (b.indexOf(e) !== -1) return true;
+    });
+}
+
+function doIntersect(a, b) {
+    return intersect(a, b).length > 0;
+}
+
+// Derive topic based on keywords in a string
+// this is meant for small corpuses of text (titles, etc ...)
+module.exports = function topic(str) {
+    var tokens = tokenize(str);
+
+    return Object.keys(topics).filter(function(key) {
+        var topicTokens = topics[key];
+
+        return doIntersect(tokens, topicTokens);
+    });
+};

c75e52e3b6d42e25ae7b53dcde7d613ac6850d55
Jesús Leganés Combarro "piranna <piranna@gmail.com>
2014-10-20 21:54:40 +0200

    Use SVG templates instead of hard-coded layout

--- draw.js
+++ draw.js
@@ -2,6 +2,8 @@ var Q = require('q');
 var fs = require('fs');
 var _ = require('lodash');
 var Canvas = require('canvas');
+var compileFromFile = require('svg-templater').compileFromFile;
+var canvg = require("canvg");
 
 var topics = require('./topic');
 var colors = require('./colors.json');
@@ -32,12 +34,10 @@ function fontSizeForDimensions(str, font, width, height, lower, upper) {
     // The font size we're guessing with
     var middle = Math.floor((upper + lower) / 2);
 
+    if(middle === lower) return middle;
+
     // Get text dimensions
     var tsize = textsize(str, middle, font);
-
-    if(middle === lower) {
-        return middle;
-    }
 
     return (
         // Are we above or below ?
@@ -49,14 +49,45 @@ function fontSizeForDimensions(str, font, width, height, lower, upper) {
     );
 }
 
-function drawBackground(ctx, options) {
-    ctx.fillStyle = options.background.color;
-    ctx.fillRect(0, 0, options.size.w, options.size.h);
-}
 
-function drawTitle(ctx, options) {
-    // Continuous top offset
-    var offset = Math.floor(options.size.h * 0.20);
+module.exports = function(output, options) {
+
+    //
+    // Default options
+    //
+
+    options = _.defaults(options || {}, {
+        "title": "",
+        "author": "",
+        "font": {
+            "size": null,
+            "family": "Arial",
+            "color": '#424242'
+        },
+        "template": "templates/default.svg",
+        "size": {
+            "w": 1800,
+            "h": 2360
+        },
+        "background": {
+            "color": '#fff'
+        }
+    });
+
+
+    //
+    // Topic color
+    //
+
+    var topic = topics(options.title)[0];
+
+    options.topic = options.topic || {};
+    options.topic.color = (topic && colors[topic]) ? colors[topic] : colors.default;
+
+
+    //
+    // Title split in lines & size
+    //
 
     // Max width that text can take
     var maxWidth = Math.floor(options.size.w * 0.8);
@@ -70,12 +101,9 @@ function drawTitle(ctx, options) {
         Math.floor(options.size.h * 0.1)
     );
 
-    var lines = parts
-    .reduce(function(lines, part) {
+    options.title = parts.reduce(function(lines, part) {
         // First part
-        if(lines.length === 0) {
-            return [part];
-        }
+        if(lines.length === 0) return [part];
 
         // Last part
         var prevPart = lines[lines.length - 1];
@@ -85,14 +113,13 @@ function drawTitle(ctx, options) {
         // Size of previous part by itself
         var fsize = fontSizeForDimensions(
             prevPart,
-            font, maxWidth, partHeight
+            options.font, maxWidth, partHeight
         );
-
 
         // How big is it if we add our new part ?
         var fsize2 = fontSizeForDimensions(
             newPart,
-            font, maxWidth, partHeight
+            options.font, maxWidth, partHeight
         );
 
         // If sizes are the same, then merge parts to same line
@@ -104,37 +131,20 @@ function drawTitle(ctx, options) {
         return lines.concat(part);
     }, []);
 
-    // Font
-    var font = options.font.family;
+    options.size.title = options.size.title
+    || Math.min.apply(Math, options.title.map(function(title)
+    {
+      return fontSizeForDimensions(
+        title, options.font.family, maxWidth, partHeight
+      );
+    }))
 
-    lines.forEach(function(part) {
-        // Font size
-        var fsize = fontSizeForDimensions(
-            part, font, maxWidth, partHeight
-        );
 
-        // Text dimensions (width & height)
-        var tdim = textsize(part, fsize, font);
+    //
+    // Author size
+    //
 
-        // Draw text
-        ctx.fillStyle = options.font.color;
-        ctx.font = fsize+"px "+font;
-        ctx.fillText(
-            // Part of title
-            part,
-            // Left Margin (center text)
-            Math.floor(options.size.w/2 - tdim.width/2),
-            // Top Margin
-            offset
-        );
-
-        // Increase offset
-        offset += tdim.height;
-    });
-}
-
-function drawAuthor(ctx, options) {
-    var fasize = fontSizeForDimensions(
+    options.size.author = options.size.author || fontSizeForDimensions(
         options.author,
         options.font.family,
 
@@ -143,110 +153,59 @@ function drawAuthor(ctx, options) {
         100
     );
 
-    // Author
-    ctx.fillStyle = options.font.color;
-    ctx.font = fasize+"px "+options.font.family;
-    ctx.fillText(
-        // Title
-        options.author,
-        // Left Margin
-        Math.floor(options.size.w * 0.1),
-        // Top Margin
-        options.size.h * 0.9
-    );
-}
 
-function drawPublished(ctx, options) {
-    var w = options.size.w;
-    var h = options.size.h;
+    //
+    // Published with GitBook image
+    //
 
-    // Get image
-    var pngData = fs.readFileSync(__dirname + '/published-with-gitbook.png');
-    img = new Canvas.Image();
-    img.src = pngData;
+    var static = require('node-static');
+    var file = new static.Server();
 
-    var imgH = Math.floor(h * 0.15);
-    var imgW = Math.floor(img.width * (imgH / img.height));
+    const port = 8080;
 
-    ctx.drawImage(
-        img,
-        // Offset x
-        w - imgW * 1.1,
-        // Offset y
-        h - imgH * 1.2,
-        // Width
-        imgW,
-        // Height
-        imgH
-    );
-}
+    var server = require('http').createServer(function (request, response) {
+      request.addListener('end', function () {
+        file.serve(request, response);
+      }).resume();
+    }).listen(port);
 
-function drawColor(ctx, options) {
-    var _topics = topics(options.title);
-    var topic = _topics[0];
+    options.autocover = options.autocover || {};
+    options.autocover.baseuri = options.autocover.baseuri || 'http://localhost:'+port;
 
-    var color = (topic && colors[topic]) ? colors[topic] : colors.default;
 
-    var hx = options.size.w / 2;
-    var hy = options.size.h * 0.7;
+    //
+    // Generate the cover
+    //
 
-    var x = 900;
-    var y = 200;
-
-    ctx.fillStyle = color;
-    ctx.fillRect(hx-x/2, hy-y/2, x, y);
-}
-
-module.exports = function(output, options) {
     var d = Q.defer();
 
-    // Default options
-    options = _.defaults(options || {}, {
-        "title": "",
-        "author": "",
-        "font": {
-            "size": null,
-            "family": "Arial",
-            "color": '#424242'
-        },
-        "size": {
-            "w": 1800,
-            "h": 2360
-        },
-        "background": {
-            "color": '#fff'
+    compileFromFile(options.template, options, function(error, data) {
+      if(error) return d.reject(error);
+
+      // Setup canvas & context
+      var canvas = new Canvas(options.size.w, options.size.h);
+
+      // Render SVG image
+      canvg(canvas, data,
+      {
+        ignoreAnimation: true,
+        renderCallback: function()
+        {
+          server.close();
+
+          // Create streams
+          var out = fs.createWriteStream(output);
+          var stream = canvas.jpegStream();
+
+          // Pipe
+          stream.pipe(out);
+
+          // Wait till finished piping/writing
+          out.on('close', d.resolve);
+          out.on('error', d.reject);
         }
+      });
     });
-
-    // Setup canvas & context
-    var canvas = new Canvas(options.size.w, options.size.h);
-    var ctx = canvas.getContext('2d');
-
-    // Background
-    drawBackground(ctx, options);
-
-    // Title
-    drawTitle(ctx, options);
-
-    // Author
-    drawAuthor(ctx, options);
-
-    // Published with GitBook
-    drawPublished(ctx, options);
-
-    // Draw the color (based on the detected topic)
-    //drawColor(ctx, options);
-
-    // Create streams
-    var out = fs.createWriteStream(output);
-    var stream = canvas.jpegStream();
-
-    // Pipe
-    stream.pipe(out);
-
-    // Wait till finished piping/writing
-    out.on('close', d.resolve);
-    out.on('error', d.reject);
 
     return d.promise;
 };

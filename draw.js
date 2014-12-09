1716495810f0c023042d8ee512ab8b1927a02b1f
Aaron O'Mullan <aaron.omullan@friendco.de>
2014-12-09 12:26:27 +0100

    Move svg rendering to individual module

--- draw.js
+++ draw.js
@@ -7,6 +7,8 @@ var canvg = require("canvg");
 
 var fontSize = require('./lib/fontsize');
 var titleParts = require('./lib/titleparts');
+var svgCompile = require('./lib/svgcompile');
+var svgRender = require('./lib/svgrender');
 
 var topics = require('./topic');
 var colors = require('./colors.json');
@@ -58,7 +60,7 @@ module.exports = function(output, options) {
         w: Math.floor(options.size.w * 0.8),
         h: Math.floor(options.size.h * 0.6),
     };
-    var maxLineHeight = Math.floor(options.size.h * 0.1);
+    var maxLineHeight = Math.floor(titleBox.h * 0.1);
 
     var tParts = titleParts(
         options.title,
@@ -102,35 +104,12 @@ module.exports = function(output, options) {
     // Generate the cover
     //
 
-    var d = Q.defer();
-
     var template = fs.existsSync('cover.svg') ? 'cover.svg' : options.template;
 
-    compileFromFile(template, options, function(error, data) {
-      if(error) return d.reject(error);
-
-      // Setup canvas & context
-      var canvas = new Canvas(options.size.w, options.size.h);
-
-      // Render SVG image
-      canvg(canvas, data,
-      {
-        ignoreAnimation: true,
-        renderCallback: function()
-        {
-          // Create streams
-          var out = fs.createWriteStream(output);
-          var stream = canvas.jpegStream();
-
-          // Pipe
-          stream.pipe(out);
-
-          // Wait till finished piping/writing
-          out.on('close', d.resolve);
-          out.on('error', d.reject);
-        }
-      });
+    // Make SVG with options
+    return svgCompile(template, options)
+    .then(function(svg) {
+        // Render SVG to JPEG
+        return svgRender(output, svg, options.size.w, options.size.h);
     });
-
-    return d.promise;
 };

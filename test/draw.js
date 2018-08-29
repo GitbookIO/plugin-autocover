731437b71ffd57a1aa681a857ec1df38f383454c
Piotr Kowalski <piecioshka@gmail.com>
2018-08-29 21:39:16 +0200

    Upgrade deps: resolve breaking compilation

--- test/draw.js
+++ test/draw.js
@@ -1,11 +1,11 @@
 var assert = require('assert');
 var draw = require('../lib/draw');
 
-describe('draw', function() {
-    it('should correctly write file', function() {
+describe('draw', function () {
+    it('should correctly write file', function () {
         return draw('./test.jpeg', {
-            title: "The Swift Programming Language 中文版",
-            author: "Samy Pessé"
+            title: 'The Swift Programming Language 中文版',
+            author: 'Samy Pessé'
         });
     });
 });
